50503b949899da524ff943038da3656bd72322f4
SergioFontan <sergiofontanllamas@gmail.com>
2024-10-08 16:21:17 +0200

    Clase 07-OCT

---
+++ clases/2024-10-07. Variables/app.js
@@ -0,0 +1,52 @@
+// console.log("hola");
+// console.log(5);
+// console.log(4+6);
+// console.log("4+6");
+
+// console.log(true);
+// console.log(false);
+
+let caja = 3; // Crear variable 'caja' con contenido 3
+caja = 6; // Cambiar el valor de la variable 'caja' a 6
+console.log(caja); // Imprimir el valor de 'caja' en la consola
+
+/**
+ * Ejercicio 1.
+ * Crear dos variables con numeros
+ * Sumar su contenido
+ * Almacenar en una tercera variable el resultado
+ */
+let valor1 = 5;
+let valor2 = 7;
+let resultado = valor1 + valor2;
+console.log(resultado);
+
+// Crear una constante
+const NOMBRE = "Sergio";
+console.log(NOMBRE);
+
+let nombre_apellidos = "Sergio Fontan Llamas"; // OK
+let nombreapellidos = "Sergio Fontan Llamas";  // Correcta pero no nos vale
+let nombreApellidos = "Sergio Fontan Llamas";  // OK (mejor opción)
+
+const PI = 3.14;
+console.log(PI + 4);
+
+/**
+ * Operadores MATEMATICOS
+ * 
+ * + realiza la suma
+ * - realiza la resta
+ * * realiza la multiplicacion
+ * / realiza la division
+ */
+
+/**
+ * Ejercicio 2.
+ * Crea dos variables/constantes con numeros (se pueden decimales).
+ * Dividelos y muestra el resultado en la consola (con o sin tercera variable).
+ */
+
+let dividendo = 4.7;
+let divisor = 0; // Al dejar el divisor como 0, el resultado es Infinito
+console.log(dividendo / divisor);
