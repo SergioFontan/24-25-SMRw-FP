ccedda290a4fa66f122f8b8996a97f05329a32bf
SergioFontan <sergiofontanllamas@gmail.com>
2024-10-15 11:11:17 +0200

    Clase 14-OCT

---
+++ clases/2024-10-14. Hoja 1. Variables/app.js
@@ -0,0 +1,26 @@
+// Ejercicio 1
+let ej1a = 4;
+let ej1b = 1;
+console.log(ej1a + ej1b);
+
+// Ejercicio 2
+let ej2a = 4;
+let ej2b = 1;
+let ej2resultado = ej2a - ej2b;
+console.log("El resultado es " + ej2resultado);
+
+// Ejercicio 3
+let ej3a = 8;
+let ej3b = 3;
+console.log(ej3a * ej3b);
+
+// Ejercicio 4
+let ej4a = 8;
+let ej4b = 2;
+let ej4resultado = 100 * ej4b / ej4a;
+console.log("El resultado es " + ej4resultado);
+
+// Ejercicio 5
+let ej5a = 2;
+let ej5b = 2;
+console.log(ej5a / ej5b);
